297af3748d41c00440a6ea7a98bff9269cb2eaac
moritzraho <raho@adobe.com>
2020-02-07 13:35:02 +0100

    fix for npm not including .gitignore template (#20)

--- generators/app/index.js
+++ generators/app/index.js
@@ -103,6 +103,13 @@ class CodeGenerator extends Generator {
       this.destinationPath(dotenvFilename),
       this.props
     )
+    // npm pack will not include .gitignore template files so we need to rename it
+    // see https://github.com/npm/npm/issues/3763
+    this.fs.copyTpl(
+      this.templatePath('_dot.gitignore'),
+      this.destinationPath('.gitignore'),
+      this.props
+    )
     // let actions and ui generator create subfolders + manifest
   }
 
