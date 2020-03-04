30488b18de01673f0d485bebd8ffc819acc0251a
Moritz Raho <raho@adobe.com>
2020-03-04 18:11:32 +0100

    add license header flag to utils

--- generators/add-action/templates/utils.js
+++ generators/add-action/templates/utils.js
@@ -7,7 +7,9 @@ Unless required by applicable law or agreed to in writing, software distributed 
 the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
-<% } %> */
+<% } %>
+* <license header>
+*/
 
 function stringParameters (params) {
   // hide authorization token without overriding params
