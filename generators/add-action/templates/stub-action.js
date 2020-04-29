50d8a255d3d200853d57045ea47a45563d647ec6
Moritz Raho <raho@adobe.com>
2020-04-29 11:10:58 +0200

    better note

--- generators/add-action/templates/stub-action.js
+++ generators/add-action/templates/stub-action.js
@@ -13,6 +13,13 @@ governing permissions and limitations under the License.
 
 /**
  * <%= description %>
+ *
+ * Note:
+ * You might want to disable authentication and authorization checks against Adobe Identity Management System for a generic action. In that case:
+ *   - Remove the require-adobe-auth annotation for this action in the manifest.yml of your application
+ *   - Remove the Authorization header from the array passed in checkMissingRequestInputs
+ *   - The two steps above imply that every client knowing the URL to this deployed action will be able to invoke it without any authentication and authorization checks against Adobe Identity Management System
+ *   - Make sure to validate these changes against your security requirements before deploying the action
  */
 
 const { Core } = require('@adobe/aio-sdk')
@@ -33,8 +40,6 @@ async function main (params) {
 
     // check for missing request input parameters and headers
     const requiredParams = <%- requiredParams %>
-    /* note: if in manifest.yml, require-adobe-auth is disabled and if this action doesn't
-      require an input token, then the Authorization header can be removed from the check below */
     const errorMessage = checkMissingRequestInputs(params, requiredParams, ['Authorization'])
     if (errorMessage) {
       // return and log client errors
