0029e8b3a7e867bc09a1d1aa88ea3f5c469cb89f
Jesse MacFadyen <purplecabbage@gmail.com>
2020-06-24 17:30:01 -0700

    add 3.0.0 @react-spectrum deps, no more rcs! (#72)
    * add 3.0.0 @react-spectrum deps, no more rcs!
    
    * update react-spectrum link
    
    * Firefly SDKs => Adobe I/O SDK
    
    * fix failing test, I love having to update text in 2 places ~sarcasm~

--- generators/add-web-assets/exc-react/templates/src/App.js
+++ generators/add-web-assets/exc-react/templates/src/App.js
@@ -206,7 +206,7 @@ export default class App extends React.Component {
             </Link>
             <Link UNSAFE_className='doc-item'>
               <a href='https://github.com/adobe/aio-sdk#adobeaio-sdk' target='_blank'>
-                Firefly SDKs
+                Adobe I/O SDK
               </a>
             </Link>
 <% if (hasBackend) { %>            <Link UNSAFE_className='doc-item'>
@@ -215,7 +215,7 @@ export default class App extends React.Component {
               </a>
             </Link><% } %>
             <Link UNSAFE_className='doc-item'>
-              <a href='https://react-spectrum.adobe.com/docs/react-spectrum/ActionButton.html' target='_blank'>
+              <a href='https://react-spectrum.adobe.com/react-spectrum/index.html' target='_blank'>
                 React Spectrum
               </a>
             </Link>
