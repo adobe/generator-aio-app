cffaed047da9415b9ab9aa0031f686ae2be7e56c
Jesse MacFadyen <purplecabbage@gmail.com>
2020-10-02 19:05:55 -0700

    cleaned up rendering, removed a bunch of messy spectrum-css

--- generators/add-web-assets/raw/templates/src/index.js
+++ generators/add-web-assets/raw/templates/src/index.js
@@ -12,8 +12,10 @@ governing permissions and limitations under the License.
 */
 
 import Runtime, { init } from '@adobe/exc-app'
-
 import actions from './config.json'
+import actionWebInvoke from './utils.js'
+
+let state = {}
 
 window.onload = () => {
   /* Here you can bootstrap your application and configure the integration with the Adobe Experience Cloud Shell */
@@ -28,45 +30,96 @@ window.onload = () => {
   }
 
   showActionsList()
+  document.getElementById('actionForm').onsubmit = (event) => {
+    event.preventDefault();
+    setTimeout(doSubmit, 1)
+  }
 }
 
 /**
  * Initialize runtime and get IMS profile
  */
-function initRuntime () {
+function initRuntime() {
   // get the Experience Cloud Runtime object
   const runtime = Runtime()
-
-  // use this to set a favicon
-  // runtime.favicon = 'url-to-favicon'
-
-  // use this to respond to clicks on the app-bar title
-  // runtime.heroClick = () => window.alert('Did I ever tell you you\'re my hero?')
-
   // ready event brings in authentication/user info
   runtime.on('ready', ({ imsOrg, imsToken, imsProfile, locale }) => {
     // tell the exc-runtime object we are done
     runtime.done()
-    console.log('Ready! received imsProfile:', imsProfile)
+    state = { imsOrg, imsToken, imsProfile, locale }
+    console.log('exc-app:ready')
   })
-
   // set solution info, shortTitle is used when window is too small to display full title
   runtime.solution = {
     icon: 'AdobeExperienceCloud',
-    title: '<%= projectName %>',
-    shortTitle: 'JGR'
+    title: 'test-raw'
   }
-  runtime.title = '<%= projectName %>'
+  runtime.title = 'test-raw'
 }
 
 /**
  * Generate list of actions
  */
-function showActionsList () {
+function showActionsList() {
   const container = document.getElementById('action-list')
   if (Object.keys(actions).length === 0) {
     container.innerHTML = '<span>you have no actions, run <code>aio app add actions</code> to add one</span>'
+  } else if (Object.keys(actions).length === 1) {
+    container.innerHTML = `<span>${actions[0].actionName}</span>`
   } else {
-    container.innerHTML = '<div>' + Object.entries(actions).map(([actionName, url]) => `<div><a href=${url} class="spectrum-Link spectrum-Link--quiet">${actionName}: ${url}</a></div>`).join('') + '</div>'
+    container.innerHTML = '<select id="selAction">' + Object.entries(actions).map(([actionName]) => `<option>${actionName}</option>`).join('') + '</select>'
   }
+}
+/**
+ * Quick helper to safely call JSON.parse
+ * @param {string} val 
+ */
+function safeParse(val) {
+  let result = null
+  try {
+    result = JSON.parse(val)
+  } catch (e) { }
+  return result
+}
+
+function doSubmit() {
+  const actionIndex = document.getElementById('selAction').selectedIndex || 0;
+  document.getElementById('taOutput').innerHTML = ""
+  if (actions) {
+    const selAction = Object.entries(actions)[actionIndex]
+    const headers = safeParse(document.getElementById('actionHeaders').value)
+    const params = safeParse(document.getElementById('actionParams').value)
+    // track the time to a result
+    const preCallTime = Date.now()
+    invokeAction(selAction, headers, params)
+      .then(actionResponse => {
+        const taOutput = document.getElementById('taOutput')
+        console.log('ActionResponse:', actionResponse)
+        taOutput.innerHTML = `${JSON.stringify(actionResponse, 0, 2)}\n\n  time:${(Date.now() - preCallTime)}ms`
+      }).catch(err => {
+        console.error('Error:', err)
+      })
+  }
+}
+
+function invokeAction(action, _headers, _params) {
+  const headers = _headers || {}
+  const params = _params || {}
+  // all headers to lowercase
+  Object.keys(headers).forEach((h) => {
+    const lowercase = h.toLowerCase()
+    if (lowercase !== h) {
+      headers[lowercase] = headers[h]
+      headers[h] = undefined
+      delete headers[h]
+    }
+  })
+  // set the authorization header and org from the ims props object
+  if (state.imsToken && !headers.authorization) {
+    headers.authorization = `Bearer ${state.imsToken}`
+  }
+  if (state.imsOrg && !headers['x-gw-ims-org-id']) {
+    headers['x-gw-ims-org-id'] = state.imsOrg
+  }
+  return actionWebInvoke(action[1], headers, params)
 }
