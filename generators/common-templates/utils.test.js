a88428ec7bfd3f1b0b72b09290b61327018a4c1a
Shazron Abdullah <shaz@adobe.com>
2021-06-24 13:46:29 +0800

    fix: shared lib relative path in shared lib test file

--- generators/common-templates/utils.test.js
+++ generators/common-templates/utils.test.js
@@ -11,7 +11,7 @@ governing permissions and limitations under the License.
 * <license header>
 */
 
-const utils = require('../../actions/utils')
+const utils = require('./<%= utilsRelPath %>')
 
 test('interface', () => {
   expect(typeof utils.errorResponse).toBe('function')
