753939456fdc1b177d925deaf89bbd9bd8ec6fb7
Moritz Raho <raho@adobe.com>
2021-10-04 17:01:31 +0200

    Rename to App Builder

--- generators/add-web-assets/exc-react/templates/src/components/About.js
+++ generators/add-web-assets/exc-react/templates/src/components/About.js
@@ -21,7 +21,7 @@ export const About = () => (
         <li>
           <Link>
             <a href='https://github.com/AdobeDocs/project-firefly/blob/master/README.md#project-firefly-developer-guide' target='_blank'>
-              Firefly Apps
+              App Builder
             </a>
           </Link>
         </li>
